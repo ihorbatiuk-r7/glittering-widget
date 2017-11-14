ea2e27cc095b6c3cd33137cd81059abb1b3aa263
Ian Horbatiuk <ian_horbatiuk@rapid7.com>
2017-11-14 13:10:16 -0500

    My First Official commit!

---
+++ shinydodad/src/NotHelloWorld.java
@@ -0,0 +1,18 @@
+/***************************************************************************
+ * COPYRIGHT (C) 2017, Rapid7 LLC, Boston, MA, USA.
+ * All rights reserved. This material contains unpublished, copyrighted
+ * work including confidential and proprietary information of Rapid7.
+ **************************************************************************/
+import java.util.stream.*;
+import java.io.*;
+
+/**
+ * I'm a basic test class for working with GitHub and IntelliJ
+ */
+public class NotHelloWorld
+{
+   public static void main(String[] args)
+   {
+      System.out.println("Death to All Humans!");
+   }
+}
